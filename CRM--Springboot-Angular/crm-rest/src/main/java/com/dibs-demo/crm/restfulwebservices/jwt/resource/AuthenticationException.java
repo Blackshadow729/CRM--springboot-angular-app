72265b1c304a05ee34243c562a4997197e992a6b
Dibyajyoti Ghosh <djghosh6@gmail.com>
2024-08-19 12:03:13 +0530

    Add files via upload
    finally added the project version control : )

---
+++ CRM--Springboot-Angular/crm-rest/src/main/java/com/dibs-demo/crm/restfulwebservices/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.dibs-demo.crm.restfulwebservices.jwt.resource;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ CRM--Springboot-Angular/crm-rest/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer (id, name, birth_date, email) VALUES (10001, 'Dibyajyoti', sysdate(), 'admin@dibs-demo.net');
+INSERT INTO customer (id, name, birth_date, email) VALUES (10002, 'harsh', sysdate(), 'harsh@dibs-demo.net');
+INSERT INTO customer (id, name, birth_date, email) VALUES (10003, 'sanchit', sysdate(), 'sanchit@dibs-demo.net');
